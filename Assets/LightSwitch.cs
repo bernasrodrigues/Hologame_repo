b7d4f2a99dc7bb6ab77d41e647ae90464a574f0e
bernasrodrigues <47544028+bernasrodrigues@users.noreply.github.com>
2023-09-26 01:34:26 +0100

    update

---
+++ Assets/LightSwitch.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour
+{
+    public List<GameObject> LightList;
+
+    /*
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (this.transform.rotation.x <= -7) {
+        
+        
+        }
+        else if (
+            this.transform.rotation.x > 7){
+
+
+            }
+        }
+    }
+    */
+}
